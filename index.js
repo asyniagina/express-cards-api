7f08dde15b2890be6edc7839a479265304a82e22
Alina <alina.sinyagina@gmail.com>
2021-07-30 15:52:29 -0700

    port

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const routes = require('./routes');
 const bodyParser = require('./bodyParser');
 
 const app = express();
-const port = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 bodyParser(app);
 routes(app);
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
