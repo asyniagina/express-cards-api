bd7708219fe3216362935f668451235e89cae90b
Alina <alina.sinyagina@gmail.com>
2021-07-31 16:23:52 -0700

    port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('./bodyParser');
+const dbConnection = require('./dbConnection');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+dbConnection();
 bodyParser(app);
 routes(app);
 
